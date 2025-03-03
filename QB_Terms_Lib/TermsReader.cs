954f2095e0f2ad732f2ed7bfc61263872e8b4804
Dgolla24 <gollad@pnw.edu>
2025-03-03 01:03:09 +0000

    Adderdone

--- QB_Terms_Lib/TermsReader.cs
+++ QB_Terms_Lib/TermsReader.cs
@@ -1,3 +1,4 @@
+using Microsoft.VisualBasic;
 using QBFC16Lib;
 using Serilog;
 // testing file update
@@ -64,7 +65,7 @@ namespace QB_Terms_Lib
 
         static List<PaymentTerm> WalkStandardTermsQueryRs(IMsgSetResponse responseMsgSet)
         {
-            List<PaymentTerm > results = new List<PaymentTerm>();
+            List<PaymentTerm> results = new List<PaymentTerm>();
             if (responseMsgSet == null) return results;
             IResponseList responseList = responseMsgSet.ResponseList;
             if (responseList == null) return results;
